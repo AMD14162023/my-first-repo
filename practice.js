76070a06288f0529b0a6b5600637d8653ab08075
AMD14162023 <anangadas2023@gmail.com>
2023-07-13 00:06:20 +0600

    I am Ono, this is my fist commit

---
+++ practice.js
@@ -0,0 +1,6 @@
+const btn2 = document.getElementById('btn2');
+const btn2Text = document.getElementById('btn2-text');
+
+btn2.addEventListener('click', () => {
+  btn2Text.classList.toggle('hidden');
+});
